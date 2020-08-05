06bc3b23312cd02e1382c981852f7fc7a9a742c1
Annalise Murphy <nali247@gmail.com>
2020-08-04 21:30:39 -0700

    transfered drop down into the Header.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,49 @@
 import React from 'react';
 import './App.css';
-import images from './data.js';
-import ImageList from './ImageList.js';
-// import ImageItem from './ImageItem.js';
+// import images from './data.js';
+// import ImageList from './ImageList.js';
 import Header from './Header.js';
 
-const options = [
-  "unilego",
-  "triceratops",
-  "narwhal",
-  "rhino",
-  "unicorn",
-  "markhor",
-  "addax",
-  "mouflon",
-  "chameleon",
-  "lizard",
-  "dragon"
-]
-
+// const options = [
+//   "unilego",
+//   "triceratops",
+//   "narwhal",
+//   "rhino",
+//   "unicorn",
+//   "markhor",
+//   "addax",
+//   "mouflon",
+//   "chameleon",
+//   "lizard",
+//   "dragon"
+// ]
 
 class App extends React.Component {
-  state = {
-    keyword: ''
-  }
+  // state = {
+  //   keyword: ''
+  // }
 
-handleDropDown = (e) => {
-  const type = e.target.value;
-  this.setState({ keyword: type })
-}
+// handleDropDown = (e) => {
+//   const type = e.target.value;
+//   this.setState({ keyword: type })
+// }
 
 render() {
-  const filteredImages = images.filter(image => image.keyword === this.state.keyword)
-  console.log(this.state);
+  // const filteredImages = images.filter(image => image.keyword === this.state.keyword)
+  // console.log(this.state);
   return (
       <>
         <Header />
-        <ImageList images={filteredImages} />
-         
-          <select onChange={this.handleDropDown}> 
+        {/* <ImageList images={filteredImages} /> */}
+          {/* <select onChange={this.handleDropDown}> 
             {
               options.map(type => <option key={type} value={type}>{type}</option>)
             }
-          </select>
+          </select> */}
       </>
     );
   }
 }
 
 export default App;
-
-// App.js imports the horned creatures data and passes it to ImageList.js (stretch goal) 
-// 1. Make list of images with captions, names, etc. using the data array
-// 2. then you're adding a dropdown to let you filter the images by their keyword
 
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react'
+import images from './data.js';
+import ImageList from './ImageList.js';
+
+const options = [
+    "unilego",
+    "triceratops",
+    "narwhal",
+    "rhino",
+    "unicorn",
+    "markhor",
+    "addax",
+    "mouflon",
+    "chameleon",
+    "lizard",
+    "dragon"
+  ]
 
 class Header extends Component {
+    state = {
+        keyword: ''
+    }
+    
+    handleDropDown = (e) => {
+      const type = e.target.value;
+      this.setState({ keyword: type })
+    }
+
     render() {
+        const filteredImages = images.filter(image => image.keyword === this.state.keyword)
+        console.log(this.state);
         return (
+            <>
             <div>
                 <header> Horned Creatures! </header>
+                <select onChange={this.handleDropDown}> 
+            {
+                options.map(type => <option key={type} value={type}>{type}</option>)
+            }
+                </select>
+            <ImageList images={filteredImages} />
             </div>
+            </>
         )
     }
 }
