22fac98cd0216c964188f46c4691ac6e6ca9c2c7
Annalise Murphy <nali247@gmail.com>
2020-08-09 22:28:03 -0700

    css added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
 import React from 'react';
-// import './App.css';
 import './styles.css';
-// import images from './data.js';
-// import ImageList from './ImageList.js';
 import Header from './Header.js';
 
-// const options = [
-//   "unilego",
-//   "triceratops",
-//   "narwhal",
-//   "rhino",
-//   "unicorn",
-//   "markhor",
-//   "addax",
-//   "mouflon",
-//   "chameleon",
-//   "lizard",
-//   "dragon"
-// ]
 
 class App extends React.Component {
-  // state = {
-  //   keyword: ''
-  // }
-
-// handleDropDown = (e) => {
-//   const type = e.target.value;
-//   this.setState({ keyword: type })
-// }
 
 render() {
-  // const filteredImages = images.filter(image => image.keyword === this.state.keyword)
-  // console.log(this.state);
   return (
       <>
         <Header />
-        {/* <ImageList images={filteredImages} /> */}
-          {/* <select onChange={this.handleDropDown}> 
-            {
-              options.map(type => <option key={type} value={type}>{type}</option>)
-            }
-          </select> */}
       </>
     );
   }
